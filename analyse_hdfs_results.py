8a6fefcc9a93255237d4ce3137cf16f4a1fff76b
Tiernan Barry <x19141840@student.ncirl.ie>
2020-04-05 23:11:07 +0100

    analysing hdfs output for 2 datasets

--- analyse_hdfs_results.py
+++ analyse_hdfs_results.py
@@ -9,44 +9,75 @@ import functions_tweet_mapreduce as fns
 import csv
 import datetime
 
-hdfs_results = pd.read_csv("/home/tiernan/PycharmProjects/DIA/output_date_sentiment")
-hdfs_results.columns = ['DATE', 'SOURCE', 'MEAN', 'STND_DEV', 'MEDIAN', 'MIN', 'MAX','COUNT']
+##########################################################
+#. Extract: Read in files from HDFS:
+##########################################################
 
-# Mean:
-# plot last 90 days:
-hdfs_results_plot90 = hdfs_results[len(hdfs_results)-40:len(hdfs_results)]
-# yy-mm
-#date_plot_format = [i[2:7] for i in hdfs_results_plot90['DATE']]
+hdfs_date_results = pd.read_csv("/home/tiernan/PycharmProjects/DIA/output_date_apr5.csv")
+hdfs_acc_results = pd.read_csv("/home/tiernan/PycharmProjects/DIA/output_acc_apr5.csv")
 
-plt.figure()
-plt.plot([i for i in range(1,len(hdfs_results_plot90['MEAN'])+1)], hdfs_results_plot90['MEAN'])
-plt.xlabel("Number of Days")
-plt.ylabel("Average Daily Sentiment")
-plt.title("X days - Average Daily Sentiment")
+##########################################################
+#. Transform:
+##########################################################
 
-# Median:
-hdfs_results_plot90 = hdfs_results[len(hdfs_results)-40:len(hdfs_results)]
-# yy-mm
-#date_plot_format = [i[2:7] for i in hdfs_results_plot90['DATE']]
+# -- Fix headings
+hdfs_date_results.columns = ['DATE', 'SOURCE', 'MEAN_SENT', 'STND_DEV_SENT', 'MEDIAN_SENT',
+       'MIN_SENT', 'MAX_SENT', 'FAVS_PER_TWEETS', 'RT_PER_TWEET', 'TWEETS_PER_DATE']
 
-plt.figure()
-plt.plot([i for i in range(1,len(hdfs_results_plot90['MEAN'])+1)], hdfs_results_plot90['MEDIAN'])
-plt.xlabel("Number of Days")
-plt.ylabel("MEDIAN Daily Sentiment")
-plt.title("X days - MEDIAN Daily Sentiment")
+hdfs_acc_results.columns = ['SOURCE', 'MEAN_SENT', 'STND_DEV_SENT', 'MEDIAN_SENT', 'MIN_SENT',
+       'MAX_SENT', 'MAX_FLWR', 'FAV_TO_FLWR', 'RT_TO_FLWR', 'TWEETS_PER_ACC']
 
-# standard deviation:
-hdfs_results_plot90 = hdfs_results[len(hdfs_results)-200:len(hdfs_results)]
-# yy-mm
-#date_plot_format = [i[2:7] for i in hdfs_results_plot90['DATE']]
+##########################################################
+#. Analysis 1: Dataset reduced by date:
+#. 1. Plot number of tweets
+#. 2. Based on above, plot summary statistics:
+#. 3.
+##########################################################
+# -- Define plotting function:
+def plot_x_last_x_days(data, stat, days, title, ylabel, pct_ch = None):
+    hdfs_results_plot90 = data[len(data)-days:len(data)]
+    # -- yy-mm
+    #date_plot_format = [i[2:7] for i in hdfs_results_plot90['DATE']]
+    plt.figure()
+    if pct_ch == "YES":
+        plt.plot([i for i in range(1,len(hdfs_results_plot90[stat])+1)], hdfs_results_plot90[stat].pct_change())
+        plt.xlabel("Number of Days")
+        plt.ylabel(ylabel)
+        plt.title(title)
+    else:
+        plt.plot([i for i in range(1, len(hdfs_results_plot90[stat]) + 1)], hdfs_results_plot90[stat])
+        plt.xlabel("Number of Days")
+        plt.ylabel(ylabel)
+        plt.title(title)
+    return plt.show()
 
-plt.figure()
-plt.plot([i for i in range(1,len(hdfs_results_plot90['MEAN'])+1)], hdfs_results_plot90['STND_DEV'])
-plt.xlabel("Number of Days")
-plt.ylabel("STND_DEV Daily Sentiment")
-plt.title("X days - STND_DEV Daily Sentiment")
+#. 1. Plot number of tweets by TWEETS_PER_DATE
+plot_x_last_x_days(hdfs_date_results, "TWEETS_PER_DATE", len(hdfs_date_results), "Number of Tweets by Day", "Number of Tweets")
 
+# -- Transform to percentage for secondary plot:
+plot_x_last_x_days(hdfs_date_results, "TWEETS_PER_DATE", len(hdfs_date_results), "Number of Tweets by Day", "Number of Tweets", "YES")
 
+#. 2. Based on above, plot summary statistics:
+plot_x_last_x_days(hdfs_date_results, "MEAN_SENT", 90, "Mean sentiment 90 days", "Mean Sentiment")
+plot_x_last_x_days(hdfs_date_results, "STND_DEV_SENT", 90, "Standard Deviation of Sentiment 90 days", "Standard Deviation")
+plot_x_last_x_days(hdfs_date_results, "FAVS_PER_TWEETS", 90, "Mean Favourite per Tweet 90 days", "Mean Favourite per Tweet")
+plot_x_last_x_days(hdfs_date_results, "RT_PER_TWEET", 90, "Mean RT per Tweet 90 days", "Mean RT")
 
+##########################################################
+#. Analysis 2: Dataset reduced by account:
+#. 1.
+#. 2.
+#. 3.
+##########################################################
 
+#. 1. Plot number of tweets by TWEETS_PER_DATE
+plot_x_last_x_days(hdfs_acc_results, "TWEETS_PER_ACC", len(hdfs_acc_results), "Number of Tweets per Account", "Number of Tweets")
 
+# -- Transform to percentage for secondary plot:
+plot_x_last_x_days(hdfs_date_results, "TWEETS_PER_DATE", len(hdfs_date_results), "Number of Tweets by Day", "Number of Tweets", "YES")
+
+#. 2. Based on above, plot summary statistics:
+plot_x_last_x_days(hdfs_date_results, "MEAN_SENT", 90, "Mean sentiment 90 days", "Mean Sentiment")
+plot_x_last_x_days(hdfs_date_results, "STND_DEV_SENT", 90, "Standard Deviation of Sentiment 90 days", "Standard Deviation")
+plot_x_last_x_days(hdfs_date_results, "FAVS_PER_TWEETS", 90, "Mean Favourite per Tweet 90 days", "Mean Favourite per Tweet")
+plot_x_last_x_days(hdfs_date_results, "RT_PER_TWEET", 90, "Mean RT per Tweet 90 days", "Mean RT")
